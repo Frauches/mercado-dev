5d1e9d53dc1cf400333f4238ef1bad66588aba3d
Matheus Frauches <frauches.27@gmail.com>
2017-12-12 23:51:49 -0200

    It has been added corrections of the previous commit

--- src/base.js
+++ src/base.js
@@ -1,17 +1,19 @@
 import Rebase from 're-base';
-import firebase from 'firebase';
-
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
     apiKey: "AIzaSyDOR7yBMrEgsjfIXM0xttu7hfWYfOwu8f0",
     authDomain: "mercado-dev-42bb6.firebaseapp.com",
     databaseURL: "https://mercado-dev-42bb6.firebaseio.com",
     projectId: "mercado-dev-42bb6",
-    storageBucket: "",
+    storageBucket: "gs://mercado-dev-42bb6.appspot.com",
     messagingSenderId: "883512687210"
 };
 
 const app = firebase.initializeApp(config);
 const base = Rebase.createClass(app.database());
 
+export const storage = app.storage();
 export default base;
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,8 @@ export default class Home extends Component {
         super(props);
 
         this.state = {
-            categorias: [],
             anuncios: []
         }
-
-        base.bindToState('categorias', {
-            context: this,
-            state: 'categorias'
-        });
 
         base.bindToState('anuncios', {
             context: this,
@@ -35,20 +29,23 @@ export default class Home extends Component {
                 <div className="container">
                     <h3>Últimos Anúncios</h3>
                     <div className="row">
-                        {this.state.anuncios.map( (anuncio, indice) => {
-                            return <AnuncioHome key={indice} anuncio={{
-                                nome: anuncio.nome,
-                                descricao: anuncio.descricao,
-                                preco: anuncio.preco,
-                                imagem: anuncio.foto
-                            }}/>;
+                        {
+
+                            Object.keys(this.state.anuncios).map( key => {
+                                const anuncio = this.state.anuncios[key];
+                                return <AnuncioHome key={key} anuncio={{
+                                    nome: anuncio.nome,
+                                    descricao: anuncio.descricao,
+                                    preco: anuncio.preco,
+                                    imagem: anuncio.foto
+                                }}/>;
 
                         })}
                     </div>
 
                     <h3>Categorias</h3>
                     <div className="row">
-                        {this.state.categorias.map( (cat, indice) => {
+                        {this.props.categorias.map( (cat, indice) => {
                             return [
                                 <LinkCategoria key={indice} categoria={{icone: cat.icon, nomeCategoria: cat.categoria}}/>,
                                 ++index%4 === 0 && <div key={'c'+indice} className="w-100"></div>
